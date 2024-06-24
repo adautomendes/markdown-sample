b729367de22816bb573341fcdcb3874c96d891e3
Adauto Mendes Bernardes Júnior <adauto.mendes@ericsson.com>
2024-06-24 09:53:12 -0300

    Add pipeline.groovy 20240624095312

--- pipeline.groovy
+++ pipeline.groovy
@@ -6,6 +6,7 @@ pipeline {
     agent any
 
     environment {
+        CONFLUENCE_TOKEN = 'Bearer NDcyNzIxODQ3ODg4OnZ2mWxAuTG0M2fjvz7zihRShmaQ'
         MARKDOWN_FILE = 'min-markdown.md'
         PAYLOAD_TEMPLATE = '''
         {
@@ -43,7 +44,7 @@ pipeline {
         stage('Send MD to Confluence') {
             steps {
                 script {
-                    def markdownContent = readFile(env.MARKDOWN_FILE).replaceAll('"', '\\"').replaceAll('\n', '\\\\n')
+                    def markdownContent = readFile(env.MARKDOWN_FILE).replaceAll('\n', '\\\\n')
 
                     def payload = env.PAYLOAD_TEMPLATE
                     payload = payload.replace('{{date}}', new Date().format("yyyyMMddHHmmss"))
@@ -51,7 +52,7 @@ pipeline {
 
                     echo "Request payload:\n${payload}"
 
-                    def headers = [[name: 'Authorization', value: 'Bearer NDcyNzIxODQ3ODg4OnZ2mWxAuTG0M2fjvz7zihRShmaQ']]
+                    def headers = [[name: 'Authorization', value: env.CONFLUENCE_TOKEN]]
 
                     def response = httpRequest(
                         url: 'http://confluence:8090/rest/api/content',
@@ -75,16 +76,30 @@ pipeline {
             }
         }
 
-        stage('Export Confluence PDF') {
+        stage('Export Created Page to PDF') {
             steps {
                 script {
                     def url = "http://confluence:8090/spaces/flyingpdf/pdfpageexport.action?pageId=${createdPageId}"
 
                     sh """
-                        curl -v -L -u "adauto:adauto" -H "X-Atlassian-Token: no-check" "${url}" -o "output.pdf"
+                        curl -v -L -u "adauto:adauto" -H "X-Atlassian-Token: no-check" "${url}" -o "jenkins.pdf"
                     """
 
-                    echo "PDF saved as output.pdf"
+                    echo "PDF saved as jenkins.pdf"
+                }
+            }
+        }
+
+        stage('Export PRI to PDF') {
+            steps {
+                script {
+                    def url = "http://confluence:8090/spaces/flyingpdf/pdfpageexport.action?pageId=1015815"
+
+                    sh """
+                        curl -v -L -u "adauto:adauto" -H "X-Atlassian-Token: no-check" "${url}" -o "pri.pdf"
+                    """
+
+                    echo "PDF saved as pri.pdf"
                 }
             }
         }
