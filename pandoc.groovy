2ef9a232a115de20a7858519498e290a833f0f2b
Adauto Mendes Bernardes Júnior <adauto.mendes@ericsson.com>
2024-07-11 15:44:56 -0300

    Update pandoc.groovy

---
+++ pandoc.groovy
@@ -0,0 +1,31 @@
+import groovy.json.JsonSlurper
+
+pipeline {
+    agent any
+
+    environment {
+        TESTE='teste'
+    }
+
+    stages {
+        stage('Docker') {
+            steps {
+                script {
+                    sh """
+                        docker ps -a
+                    """
+                }
+            }
+        }
+    }
+
+    post {
+        failure {
+            echo 'The pipeline failed.'
+        }
+        success {
+            echo 'Pipeline concluded successfully!'
+            archiveArtifacts artifacts: '**/*.pdf', allowEmptyArchive: true
+        }
+    }
+}
