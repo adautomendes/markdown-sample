2dd3cd9ee133460da63c5ed594bf3f8ed840239f
Adauto Mendes Bernardes Júnior <adauto.mendes@ericsson.com>
2024-07-11 15:46:19 -0300

    Update pandoc.groovy

--- pandoc.groovy
+++ pandoc.groovy
@@ -13,6 +13,7 @@ pipeline {
                 script {
                     sh """
                         docker ps -a
+                        ls $(pwd)
                     """
                 }
             }
