e2370cac8cc33ff4494d96946766216f8e9b0cf1
Adauto Mendes Bernardes Júnior <adauto.mendes@ericsson.com>
2024-07-12 11:34:30 -0300

    Update pandoc script

--- pandoc.groovy
+++ pandoc.groovy
@@ -17,30 +17,46 @@ pipeline {
                 }
             }
         }
-        stage('Convert MD to HTML') {
-            steps {
-                script {
-                    sh """
-                        pandoc markdown-sample.md -o markdown-sample.html
-                    """
+        stage('Convert input file') {
+            parallel {
+                stage('Convert MD to HTML') {
+                    steps {
+                        script {
+                            sh """
+                                pandoc ${inputFile} -o out-markdown-sample.html
+                            """
+                        }
+                    }
                 }
-            }
-        }
-        stage('Convert MD to PDF') {
-            steps {
-                script {
-                    sh """
-                        pandoc markdown-sample.md -o markdown-sample.pdf
-                    """
+                stage('Convert MD to PDF') {
+                    steps {
+                        script {
+                            sh """
+                                pandoc ${inputFile} -o out-markdown-sample.pdf
+                            """
+                        }
+                    }
                 }
-            }
-        }
-        stage('Convert MD to DOCX') {
-            steps {
-                script {
-                    sh """
-                        pandoc markdown-sample.md -o markdown-sample.docx
-                    """
+                stage('Convert MD to DOCX') {
+                    steps {
+                        script {
+                            sh """
+                                pandoc ${inputFile} -o out-markdown-sample.docx
+                            """
+                        }
+                    }
+                }
+                stage('Custom convert') {
+                    steps {
+                        script {
+                            if (!fileExists(inputFile)) {
+                                error "File ${inputFile} was not found!"
+                            }
+                            sh """
+                                pandoc ${inputFile} -o out-markdown-sample.${outputType}
+                            """
+                        }
+                    }
                 }
             }
         }
@@ -51,7 +67,7 @@ pipeline {
         }
         success {
             echo 'Pipeline concluded successfully!'
-            archiveArtifacts artifacts: '**/*.html,**/*.pdf,**/*.docx', allowEmptyArchive: true
+            archiveArtifacts artifacts: '**/*.*', allowEmptyArchive: true
         }
     }
 }
